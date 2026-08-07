a438cd72bdd239f1b4d391b22dff541c838959fe
ianballou <ianballou67@gmail.com>
2026-08-07 12:02:35 -0400

    Fixes #39597 - use smart-proxy methods to trust Foreman
    * ARGV workaround to get tests passing

--- lib/smart_proxy_container_gateway/foreman_api.rb
+++ lib/smart_proxy_container_gateway/foreman_api.rb
@@ -1,27 +1,18 @@
-require 'uri'
-require 'openssl'
+require 'proxy/request'
 
 module Proxy
   module ContainerGateway
-    class ForemanApi
+    class ForemanApi < ::Proxy::HttpRequest::ForemanRequest
       def registry_request(auth_header, params, suffix, uuid: '', cert: false)
-        uri = URI.join(Proxy::SETTINGS.foreman_url, Proxy::ContainerGateway::Plugin.settings.katello_registry_path, suffix)
-        uri.query = process_params(params)
+        path = "#{Proxy::ContainerGateway::Plugin.settings.katello_registry_path}#{suffix}"
+        query = params.slice('scope', 'account').compact
 
-        req = Net::HTTP::Get.new(uri)
-        req.add_field('Authorization', auth_header) unless cert
-        req.add_field('Accept', 'application/json')
-        req.add_field('HostUUID', uuid) if cert
-        req.content_type = 'application/json'
-        http = Net::HTTP.new(uri.hostname, uri.port)
-        http.use_ssl = true
+        headers = {}
+        headers['Authorization'] = auth_header unless cert
+        headers['HostUUID'] = uuid if cert
 
-        http.request(req)
-      end
-
-      def process_params(params_in)
-        params = params_in.slice('scope', 'account').compact
-        URI.encode_www_form(params)
+        req = request_factory.create_get(path, query, headers)
+        send_request(req)
       end
 
       def fetch_token(auth_header, params)
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,9 @@
 require 'test/unit'
 $LOAD_PATH << File.join(__dir__, '..', 'lib')
 
+# Workaround: sinatra/main.rb parses ARGV at require time and exits on -v.
+# Clear ARGV before loading to prevent OptionParser from intercepting test flags.
+saved_argv = ARGV.dup
+ARGV.clear
 require 'smart_proxy_for_testing'
+ARGV.replace(saved_argv)
